23029ed94176ac77d665aaac1039456b12b9e715
MaciejCygal <58751642+MaciejCygal@users.noreply.github.com>
2020-05-21 21:43:22 +0200

    update świr paleta
    change colors

--- P2P-ecrypted-java-chat/src/org/messages/windows/style/Palette.java
+++ P2P-ecrypted-java-chat/src/org/messages/windows/style/Palette.java
@@ -11,4 +11,9 @@ public interface Palette {
 	
 	final Color ACCEPT = new Color(56,142,60);
 	final Color ACCEPT_2 = new Color(232,245,227);
+ final Color COLOR_3 = new Color(206, 90, 100);
+
+      final Color COLOR_4 = new Color(94, 8, 100);
+
+      final Color COLOR_5 = new Color(20, 55, 100);
 }
